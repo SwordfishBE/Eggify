abaf5c123e9cfe1be873ae16dbb4e2dacbe6ac6f
Jimmy A. <6824251+SwordfishBE@users.noreply.github.com>
2026-09-07 14:15:07 +0200

    Update 26.3-pre-1
    Update 26.3-pre-1

--- src/client/java/net/eggify/EggifyModMenuIntegration.java
+++ src/client/java/net/eggify/EggifyModMenuIntegration.java
@@ -3,14 +3,29 @@ package net.eggify;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.gui.screens.Screen;
 
 public final class EggifyModMenuIntegration implements ModMenuApi {
+    private static boolean warnedConfigScreenUnavailable;
+
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         if (!FabricLoader.getInstance().isModLoaded("cloth-config")) {
             return parent -> null;
         }
 
-        return EggifyClothConfigScreen::create;
+        return EggifyModMenuIntegration::createConfigScreen;
+    }
+
+    private static Screen createConfigScreen(Screen parent) {
+        try {
+            return EggifyClothConfigScreen.create(parent);
+        } catch (LinkageError exception) {
+            if (!warnedConfigScreenUnavailable) {
+                warnedConfigScreenUnavailable = true;
+                EggifyMod.LOGGER.warn("{} Cloth Config is installed but incompatible with this Minecraft version; the ModMenu config screen is disabled.", EggifyMod.LOG_PREFIX, exception);
+            }
+            return null;
+        }
     }
 }
